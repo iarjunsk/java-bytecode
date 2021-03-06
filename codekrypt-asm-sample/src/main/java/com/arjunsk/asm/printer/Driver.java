c84dc7f8a0bbe84a1b229229adf7d2cb7d3efdb4
Arjun SK <arjun.sunilkumar@nissanmotor.com>
2021-03-06 22:32:45 +0530

    Added output

--- codekrypt-asm-sample/src/main/java/com/arjunsk/asm/printer/Driver.java
+++ codekrypt-asm-sample/src/main/java/com/arjunsk/asm/printer/Driver.java
@@ -3,6 +3,20 @@ package com.arjunsk.asm.printer;
 import java.io.IOException;
 import org.objectweb.asm.ClassReader;
 
+/**
+ * OUTPUT:
+ *
+ * <pre>
+ * visit() : 52 33 com/arjunsk/asm/printer/model/RadioButton null java/lang/Object interfaces:
+ * visitSource() : RadioButton.java null
+ * visitField() : 18 text Ljava/lang/String; null null
+ * visitField() : 18 isClickable Z null null
+ * visitMethod() : 1 <init> (Ljava/lang/String;Z)V null null
+ * visitMethod() : 1 getText ()Ljava/lang/String; null null
+ * visitMethod() : 1 isClickable ()Z null null
+ * visitEnd() :
+ * </pre>
+ */
 public class Driver {
   public static void main(String[] args) throws IOException {
     ClassPrinter classPrinter = new ClassPrinter();
