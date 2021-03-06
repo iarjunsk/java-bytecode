ff144dfdbd321599653893aa007e5e83e087c65f
Arjun SK <arjun.sunilkumar@nissanmotor.com>
2021-03-06 23:53:05 +0530

    Refactor

--- codekrypt-asm-sample/src/main/java/com/arjunsk/asm/filter/sample2/ClassFilterDriver2.java
+++ codekrypt-asm-sample/src/main/java/com/arjunsk/asm/filter/sample2/ClassFilterDriver2.java
@@ -2,7 +2,6 @@ package com.arjunsk.asm.filter.sample2;
 
 import com.arjunsk.asm.filter.sample2.support.ChangeVersionAdapter;
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 
 public class ClassFilterDriver2 {
@@ -14,9 +13,9 @@ public class ClassFilterDriver2 {
 
     // classVisitor forwards all events to classWriter
     ClassWriter classWriter = new ClassWriter(0);
-    ClassVisitor classVisitor = new ChangeVersionAdapter(classWriter);
+    ChangeVersionAdapter changeVersionAdapter = new ChangeVersionAdapter(classWriter);
 
-    classReader.accept(classVisitor, 0);
+    classReader.accept(changeVersionAdapter, 0);
 
     byte[] b2 = classWriter.toByteArray(); // b2 represents the same class as b1
   }
