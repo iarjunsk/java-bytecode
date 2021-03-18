eba25662f33a9136b33e90ddebf72d92b34ee1ce
Arjun SK <arjun.sunilkumar@nissanmotor.com>
2021-03-18 17:25:05 +0530

    Added Java Docs and explanation for Cw(Cr,0)

--- java-asm/ow2-asm-example/src/main/java/com/arjunsk/asm/filter/allclasses/AllClassTransformDriver.java
+++ java-asm/ow2-asm-example/src/main/java/com/arjunsk/asm/filter/allclasses/AllClassTransformDriver.java
@@ -7,33 +7,48 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 
 /**
- * To introduce all class Transformation, we might need premain().
  *
- * <p>Eg:- Lombok
  *
- * <p>Refer 1: `ASM Guide` Page 21. (PS: I have modified ClassWriter to accept flag only argument)
+ * <pre>
+ * To perform Transformation on all the classes, we might need to use premain().
+ * Eg:- Lombok
  *
- * <p>Refer 2:
- * https://stackoverflow.com/questions/27851588/what-is-premain-and-how-does-it-get-called
+ * Refer 1: `ASM Guide` Page 21.
+ * Refer 2: https://stackoverflow.com/questions/27851588/what-is-premain-and-how-does-it-get-called
+ * </pre>
+ *
+ * <p>NOTE : In the asm guide there is an optimization performed. ClassWriter has reference to
+ * ClassReader. This optimization is two times faster than the previous one {@link
+ * com.arjunsk.asm.filter.visiterimpl.ClassFilterDriver2}, because ChangeVersionAdapter does not
+ * transform any method.
+ *
+ * <pre>
+ *    byte[] b1 = ...
+ *    ClassReader cr = new ClassReader(b1);
+ *    ClassWriter cw = new ClassWriter(cr, 0);
+ *    ChangeVersionAdapter ca = new ChangeVersionAdapter(cw);
+ *    cr.accept(ca, 0);
+ *    byte[] b2 = cw.toByteArray();
+ * </pre>
  */
 public class AllClassTransformDriver {
 
-  // Flow: CR --> CVA --> CW --> toByteArray()
+  // Flow: (CR<->CW) --> CVA --> CR --> CW.toByteArray()
   public static void premain(String agentArgs, Instrumentation inst) {
     inst.addTransformer(
         (classLoader, name, aClass, domain, bytes) -> {
 
-          // 1. CW
-          ClassWriter cw = new ClassWriter(0);
+          // 1. CR <-> CW
+          ClassReader cr = new ClassReader(bytes);
+          ClassWriter cw = new ClassWriter(cr, 0);
 
           // 2. CVA
           ClassVisitor cv = new ChangeVersionAdapter(cw);
 
           // 3. CR
-          ClassReader cr = new ClassReader(bytes);
           cr.accept(cv, 0);
 
-          // Lazy function. The final chain of invocation will only start at toByteArray() call.
+          // 4. CW
           return cw.toByteArray();
         });
   }
